ae2b2d348cc06c9a1b42d1db824160ef2dd9ea23
Chris <chris@tgu-ltd.uk>
2019-05-04 03:52:50 +0100

    Added main esp32 code and rshell script to copy files over

--- esp32/main.py
+++ esp32/main.py
@@ -1,14 +1,15 @@
 import time
-from lora.sx127x import SX127x
-from lora.controller_esp32 import ESP32Controller
+from sx127x import SX127x
+from controller_esp32 import ESP32Controller
 
 
 def get_lora():
+    ''' Setup and return a LoRa Esp32 controller'''
     params = {
         'frequency': 868E6,  # 169E6, 433E6, 434E6, 866E6, 868E6*, 915E6
-        'tx_power_level': 2,  # 2
-        'signal_bandwidth': 125E3,  # 7.8E3, 10.4E3, 15.6E3, 20.8E3, 31.25E3, 41.7E3, 62.5E3, 125E3*, 250E3
-        'spreading_factor': 8,
+        'tx_power_level': 5,  # 2
+        'signal_bandwidth': 20.8E3,  # 7.8E3, 10.4E3, 15.6E3, 20.8E3, 31.25E3, 41.7E3, 62.5E3, 125E3*, 250E3
+        'spreading_factor': 5,
         'coding_rate': 5,
         'preamble_length': 12,
         'implicitHeader': False,
@@ -24,14 +25,19 @@ def get_lora():
 
 
 def main():
+    ''' Send 10 LoRa Chirps out and write to file '''
+    output = []
+    loop = range(10)
     lora = get_lora()
-    for i in range(10):
+    for i in loop:
         payload = "{0}".format(i)
         lora.println(payload)
-        with open('lora_output.txt', 'w') as f:
-            f.write("{0} RSSI: {1}".format(payload, lora.packetRssi()))
+        rssi = '{0} RSSI: {1}\n'.format(payload, lora.packetRssi())
+        output.append(rssi)
         time.sleep(1)
+    with open('./lora_output.txt', 'w') as f:
+        f.write(''.join(output))
 
 
-if __name__ == '__main__':
+if __name__ in ['__main__', 'main']:
     main()
